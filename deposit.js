d3132823ee13502c40b10a0c9e3f2a091883a065
Mousamia <87888309+Mousamia@users.noreply.github.com>
2024-03-02 19:57:42 +0600

    deposit portion done

--- deposit.js
+++ deposit.js
@@ -1,11 +1,18 @@
+// deposit portion
 document.getElementById('deposit-btn').addEventListener('click', function(){
     let depositAmount = document.getElementById('deposit-field').value;
-    console.log( typeof depositAmount);
 
     let depositAreaField = document.getElementById('deposit-area');
     const depositPrevious = parseFloat(depositAreaField.innerText);
     depositAreaField.innerText = parseFloat(depositAmount) + depositPrevious ;
-
     document.getElementById('deposit-field').value = '';
 
+    const balanceArea = document.getElementById('balance-area');
+    console.log(balanceArea);
+    const previousBalance = parseFloat(balanceArea.innerText);
+    console.log(previousBalance);
+    balanceArea.innerText = previousBalance + parseFloat(depositAmount);
 })
+
+
+// 
